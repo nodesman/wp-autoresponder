264e359d85bea3d3434b3a87a1b2b92da89fd91b
Raj <flarecore@gmail.com>
2012-10-28 16:34:03 +0530

    Added wordpress-tests files to tests directory

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,11 +2,11 @@
 // Load the test environment
 // https://github.com/nb/wordpress-tests
 
-$path = '/usr/lib/wordpress-tests/bootstrap.php';
+$path = 'tests/wordpress-tests/bootstrap.php';
 
 if (file_exists($path)) {
         $GLOBALS['wp_tests_options'] = array(
-                'active_plugins' => array('plugin-name/main-plugin-file.php')
+                'active_plugins' => array()
         );
         require_once $path;
 } else {

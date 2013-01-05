6129c2c541de5f7899a87c683b651c1396cc5cd5
Raj <flarecore@gmail.com>
2013-01-05 19:21:38 +0530

    Corrected path to sample config file in installer

--- scripts/install-wordpress.sh
+++ scripts/install-wordpress.sh
@@ -2,6 +2,6 @@ git clone git://github.com/WordPress/WordPress.git wordpress
 cd wordpress
 last_tag=$(git tag | tail -1);
 git reset --hard $last_tag
-cp scripts/wp-config.php wordpress/
+cp ./scripts/wp-config.php wordpress/
 wp core install --blog=12.0.0.1 --email=admin@127.0.0.1 --db-name=wordpress --db-user=root --db-pass="" --db-host=127.0.0.1
 

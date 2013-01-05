f4fffdad9e70965ae57b70d5caa10198de9fd53b
Raj <flarecore@gmail.com>
2013-01-05 22:07:42 +0530

    Debuggin installation

--- scripts/wpr-activator.php
+++ scripts/wpr-activator.php
@@ -7,6 +7,12 @@ if (is_file(__DIR__."/wp-content/plugins/wp-responder-email-autoresponder-and-ne
    var_dump(activate_plugin(__DIR__."/wp-content/plugins/wp-responder-email-autoresponder-and-newsletter-plugin/wpresponder.php"));
 
     print_r(get_option('active_plugins'));
+    mysql_connect("127.0.0.1", "root", "");
+    mysql_select_db("myapp_test");
+    $res = mysql_query("SHOW TABLES;");
+    while ($table = mysql_fetch_object($res)) {
+      print_r($table);
+   }
 }
 else {
    echo "File not found";

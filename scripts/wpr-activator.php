329801a52eabf6e85575adcf46a49f3338296d88
Raj <flarecore@gmail.com>
2013-01-05 21:17:55 +0530

    Corrected syntax error in activator

--- scripts/wpr-activator.php
+++ scripts/wpr-activator.php
@@ -4,4 +4,9 @@ include __DIR__."/wp-admin/includes/plugin.php";
 echo "About to install ".__DIR__."/wp-content/plugins/wp-responder-email-autoresponder-and-newsletter-plugin/wpresponder.php";
 if (is_file(__DIR__."/wp-content/plugins/wp-responder-email-autoresponder-and-newsletter-plugin/wpresponder.php")) {
    echo "Plugin file found"; 
-activate_plugin(__DIR__."/wp-content/plugins/wp-responder-email-autoresponder-and-newsletter-plugin/wpresponder.php");
+   activate_plugin(__DIR__."/wp-content/plugins/wp-responder-email-autoresponder-and-newsletter-plugin/wpresponder.php");
+}
+else {
+   echo "File not found";
+   exit(1);
+}

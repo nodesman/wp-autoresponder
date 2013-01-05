502ab4b4fcf5517111ff407f80b730b5a00f236a
Raj <flarecore@gmail.com>
2013-01-06 00:52:01 +0530

    Fixed hash method

--- scripts/wpr-activator.php
+++ scripts/wpr-activator.php
@@ -6,7 +6,7 @@ if (is_file(__DIR__."/wp-content/plugins/wp-responder-email-autoresponder-and-ne
    echo "Plugin file found"; 
    activate_plugin(__DIR__."/wp-content/plugins/wp-responder-email-autoresponder-and-newsletter-plugin/wpresponder.php");
    $option = get_option("db_version");
-   $hash = md5($option);
+   $hash = sha1($option);
    file_put_contents(".wp-tests-version", $hash);
 }
 else {

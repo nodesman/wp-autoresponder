208439b68fcb972dcc9579e0f28b4e38a4aa9cad
Raj <flarecore@gmail.com>
2013-01-05 21:47:22 +0530

    Dumping activation result

--- scripts/wpr-activator.php
+++ scripts/wpr-activator.php
@@ -4,7 +4,7 @@ include __DIR__."/wp-admin/includes/plugin.php";
 echo "About to install ".__DIR__."/wp-content/plugins/wp-responder-email-autoresponder-and-newsletter-plugin/wpresponder.php";
 if (is_file(__DIR__."/wp-content/plugins/wp-responder-email-autoresponder-and-newsletter-plugin/wpresponder.php")) {
    echo "Plugin file found"; 
-   activate_plugin(__DIR__."/wp-content/plugins/wp-responder-email-autoresponder-and-newsletter-plugin/wpresponder.php");
+   var_dump(activate_plugin(__DIR__."/wp-content/plugins/wp-responder-email-autoresponder-and-newsletter-plugin/wpresponder.php"));
 }
 else {
    echo "File not found";

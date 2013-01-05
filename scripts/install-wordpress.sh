32386a8101fa060246653bbed8fbfb7c933e033d
Raj <flarecore@gmail.com>
2013-01-05 23:57:03 +0530

    Fixed siteurl param of wpcli

--- scripts/install-wordpress.sh
+++ scripts/install-wordpress.sh
@@ -8,6 +8,6 @@ git clone --recurse-submodules git://github.com/andreascreten/wp-cli.git ./wp-cl
 cd ./wp-cli  
 sudo utils/build-dev
 cd ../wordpress
-wp core install --blog=12.0.0.1 --email=admin@127.0.0.1 --db-name=wordpress --db-user=root --db-pass="" --db-host=127.0.0.1 --site_url=http://localhost/wordpress/ --site_title="Brown Fox" --admin_email="raj@wpresponder.com" --admin_password="password"
+wp core install --blog=12.0.0.1 --email=admin@127.0.0.1 --db-name=wordpress --db-user=root --db-pass="" --db-host=127.0.0.1 --site_url="http://localhost/wordpress/" --site_title="Brown Fox" --admin_email="raj@wpresponder.com" --admin_password="password"
 cp -r ../src ./wp-content/plugins/wp-responder-email-autoresponder-and-newsletter-plugin
 cp -v ../scripts/wpr-activator.php .

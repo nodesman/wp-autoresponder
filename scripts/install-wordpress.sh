d65c3c91ed11ea9c54298e58d14755867527987b
Raj <flarecore@gmail.com>
2013-01-05 20:24:02 +0530

    Really working?

--- scripts/install-wordpress.sh
+++ scripts/install-wordpress.sh
@@ -9,5 +9,5 @@ cd ./wp-cli
 sudo utils/build-dev
 cd ../wordpress
 wp core install --blog=12.0.0.1 --email=admin@127.0.0.1 --db-name=wordpress --db-user=root --db-pass="" --db-host=127.0.0.1 --site_url=http://localhost/wordpress/ --site_title="Brown Fox" --admin_email="raj@wpresponder.com" --admin_password="password"
-cp -r ../src ./wp-content/plugins/wp-responder-email-autoresponder-and-newsletter-plugin
+cp -r ../src ./wp-content/plugins/wp-responder-email-autoresponder-and-newsletter-plugi
 wp plugin toggle wp-responder-email-autoresponder-and-newsletter-plugin

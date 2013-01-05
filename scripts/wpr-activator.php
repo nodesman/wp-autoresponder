90eeb9be150cb1b661788575388abb17c81feb65
Raj <flarecore@gmail.com>
2013-01-05 20:59:46 +0530

    Added debuggin messages to activator

--- scripts/wpr-activator.php
+++ scripts/wpr-activator.php
@@ -1,5 +1,7 @@
 <?php 
 include __DIR__."/wp-config.php";
 include __DIR__."/wp-admin/includes/plugin.php";
-
+echo "About to install ".__DIR__."/wp-content/plugins/wp-responder-email-autoresponder-and-newsletter-plugin/wpresponder.php";
+if (is_file(__DIR__."/wp-content/plugins/wp-responder-email-autoresponder-and-newsletter-plugin/wpresponder.php")) {
+   echo "Plugin file found"; 
 activate_plugin(__DIR__."/wp-content/plugins/wp-responder-email-autoresponder-and-newsletter-plugin/wpresponder.php");

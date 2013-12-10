1f01529c899437729ce20c4a8a84196a289e8768
Raj <flarecore@gmail.com>
2013-12-11 00:25:04 +0530

    Fixed dbpassword not configurable by the environment for phpunit

--- tests/wordpress-tests/unittests-config.php
+++ tests/wordpress-tests/unittests-config.php
@@ -2,11 +2,23 @@
 /* Path to the WordPress codebase you'd like to test. Add a backslash in the end. */
 $configuration = parse_ini_file(__DIR__."/../../install.properties");
 
-define( 'ABSPATH', $configuration['wp.dir']."/");
-define('DB_NAME', $configuration['wp.dbname']);
-define( 'DB_USER', $configuration['wp.dbuser'] );
-define('DB_PASSWORD',$configuration['wp.dbpass']);
-define( 'DB_HOST',  $configuration['wp.dbhost']);
+$wp_dir = getenv('wp_dir');
+$wp_dbname = getenv('wp_dbname');
+$wp_dbuser = getenv('wp_dbuser');
+$wp_dbpass = getenv('wp_dbpass');
+$wp_dbhost = getenv('wp_dbhost');
+
+$dir = (!empty($wp_dir))?$wp_dir:$configuration['wp.dir'];
+$dbname = (!empty($wp_dbname))?$wp_dbname:$configuration['wp.dbname'];
+$user = (!empty($wp_dbuser))?$wp_dbuser:$configuration['wp.dbuser'];
+$pass = (!empty($wp_dbpass))?$wp_dbpass:$configuration['wp.dbpass'];
+$host = (!empty($wp_dbhost))?$wp_dbhost:$configuration['wp.dbhost'];
+define( 'ABSPATH', $dir."/");
+define('DB_NAME', $dbname);
+define( 'DB_USER', $user);
+define('DB_PASSWORD',$pass);
+define( 'DB_HOST',  $host);
+
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
 

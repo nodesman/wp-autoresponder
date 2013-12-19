92fb6eeb6b3a2551563dd16af1b7b8eef0a80aff
Raj S <flarecore@gmail.com>
2013-12-19 23:26:32 +0530

    Added confirmed newsletter subscribers iterator

---
+++ src/models/iterators/ConfirmedNewsletterSubscribers.php
@@ -0,0 +1,98 @@
+<?php
+
+class ConfirmedNewsletterSubscribers implements Iterator, Countable {
+
+    private $nid;
+    private $index;
+    private $length;
+
+    public function __construct($nid) {
+        global $wpdb;
+        $this->nid = intval($nid);
+        $this->getRecordsetSize();
+        $this->index = -1;
+        $this->next();
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Return the current element
+     * @link http://php.net/manual/en/iterator.current.php
+     * @return mixed Can return any type.
+     */
+    public function current()
+    {
+        global $wpdb;
+        $getSubscriberQuery = sprintf("SELECT id FROM %swpr_subscribers WHERE nid=%d LIMIT %d, 1;", $wpdb->prefix, $this->nid, $this->index);
+        $subscriber_id = $wpdb->get_var($getSubscriberQuery);
+        return new Subscriber($subscriber_id);
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Move forward to next element
+     * @link http://php.net/manual/en/iterator.next.php
+     * @return void Any returned value is ignored.
+     */
+    public function next()
+    {
+        $this->index += 1;
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Return the key of the current element
+     * @link http://php.net/manual/en/iterator.key.php
+     * @return mixed scalar on success, or null on failure.
+     */
+    public function key()
+    {
+        return $this->index;
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Checks if current position is valid
+     * @link http://php.net/manual/en/iterator.valid.php
+     * @return boolean The return value will be casted to boolean and then evaluated.
+     * Returns true on success or false on failure.
+     */
+
+    public function valid()
+    {
+        return ($this->index < $this->length && $this->index >=0);
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Rewind the Iterator to the first element
+     * @link http://php.net/manual/en/iterator.rewind.php
+     * @return void Any returned value is ignored.
+     */
+    public function rewind()
+    {
+        $this->index=-1;
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.1.0)<br/>
+     * Count elements of an object
+     * @link http://php.net/manual/en/countable.count.php
+     * @return int The custom count as an integer.
+     * </p>
+     * <p>
+     * The return value is cast to an integer.
+     */
+    public function count()
+    {
+        return $this->length;
+    }
+
+
+    private function getRecordsetSize()
+    {
+        global $wpdb;
+        $getNumberOfSubscribersQuery = sprintf("SELECT COUNT(*) number_of_subscribers FROM `%swpr_subscribers` WHERE `nid`=%d AND `active`=1 AND `confirmed`=1 ORDER BY `id` ASC;", $wpdb->prefix, $this->nid);
+        $this->length = $wpdb->get_var($getNumberOfSubscribersQuery);
+    }
+}
---
+++ tests/NewsletterSubscribersIteratorTest.php
@@ -0,0 +1,60 @@
+<?php
+include __DIR__ . "/../src/models/iterators/ConfirmedNewsletterSubscribers.php";
+
+class NewsletterSubscribersIteratorTest extends WP_UnitTestCase {
+
+    private $newsletter_id;
+    private $subscribers;
+
+    public function setUp() {
+
+        //create newsletter
+        global $wpdb;
+
+        $newsletter = array(
+            "name" => md5(microtime()),
+            "reply_to" => 'flarecore@'.md5(microtime()).'.com',
+            "fromname" => 'Test',
+            "fromemail" => 'flare@'.md5(microtime()).'.com'
+        );
+
+        $addNewsletterQuery = sprintf("INSERT INTO %swpr_newsletters (`name`, `reply_to`, `fromname`, `fromemail`) VALUES ('%s','%s', '%s', '%s')", $wpdb->prefix, $newsletter['name'], $newsletter['reply_to'], $newsletter['fromname'], $newsletter['fromemail']);
+        $wpdb->query($addNewsletterQuery);
+        $this->newsletter_id = $wpdb->insert_id;
+
+        $this->subscribers = array();
+
+        for ($iter=0; $iter<5; $iter++) {
+            $current = array(
+                "nid" => $this->newsletter_id,
+                "name" => md5("sub".microtime().$iter),
+                "email" => md5('email'.microtime().$iter),
+                "hash" => md5("hash".microtime().$iter)
+            );
+            $this->subscribers[] = $current;
+            $addSubscriberQuery = sprintf("INSERT INTO %swpr_subscribers (nid, name, email, hash, active, confirmed) VALUES (%d, '%s','%s', '%s', 1, 1);", $wpdb->prefix, $this->newsletter_id,$current['name'] , $current['email'] , $current['hash'] );
+            $wpdb->query($addSubscriberQuery);
+        }
+
+
+    }
+
+    public function testWhetherFetchingFirstItemFetchesTheFirstItem() {
+
+        $ns_iterator = new ConfirmedNewsletterSubscribers($this->newsletter_id);
+
+        $this->assertEquals(5, count($ns_iterator));
+
+        foreach ($ns_iterator as $index=> $value) {
+
+            if ($index == 0) {
+                $this->assertEquals($this->subscribers[0]['name'], $value->getName());
+            }
+
+            if ($index == 4) {
+                $this->assertEquals($this->subscribers[4]['name'], $value->getName());
+            }
+        }
+    }
+
+} 

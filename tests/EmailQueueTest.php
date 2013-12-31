587c0a6833b7ba88868d8977dbe7f1603c8f8af9
Raj S <flarecore@gmail.com>
2013-12-31 15:37:51 +0530

    Added custom fields test to the email queue manager

--- tests/EmailQueueTest.php
+++ tests/EmailQueueTest.php
@@ -29,14 +29,52 @@ class EmailQueueTest extends WP_UnitTestCase
             "meta_key" => "TEST-TEST-TEST"
         );
 
-        $expectedHtmlBody = sprintf('%s<br />%s<br /><a href="%s">Click here to unsubscribe.</a>', $email['htmlbody'], $this->senderAddressSetting, $this->subscriber->getUnsubscriptionUrl());
-        $expectedTextBody = sprintf("%s\r\n%s\r\nClick here to unsubscribe:\r\n%s", $email['textbody'], JavelinConfig::senderAddress(), $this->subscriber->getUnsubscriptionUrl());
+        $expectedHtmlBody = $this->getExpectedHtmlBody($email);
+        $expectedTextBody = $this->getExpectedTextBody($email);
 
         $emailObj = $javelinQueue->enqueue($this->subscriber, $email);
         $this->assertEquals($email['subject'], $emailObj->getSubject());
         $this->assertEquals($expectedHtmlBody, $emailObj->getHtmlBody());
         $this->assertEquals($expectedTextBody, $emailObj->getTextBody());
         $this->assertEquals($email['meta_key'], $emailObj->getMetaKey());
+    }
+
+    public function testWhetherCustomFieldValuesAreReplaced()
+    {
+        global $javelinQueue, $wpdb;
+
+        //create custom field for the newsletter
+        $createCustomFieldQuery = sprintf("INSERT INTO %swpr_custom_fields (`nid`, `type`, `name`, `label`) VALUES (%d, 'text', 'test', 'Test');", $wpdb->prefix, $this->newsletter->getId());
+        $wpdb->query($createCustomFieldQuery);
+        $customFieldId = $wpdb->insert_id;
+
+        //add the custom field value for the subscriber
+        $customFieldValue = 'MarsellusWallace';
+        $createCustomFieldValueQuery = sprintf("INSERT INTO %swpr_custom_fields_values (`nid`, `sid`, `cid`, `value`) VALUES (%d, %d, %d, '%s')",$wpdb->prefix, $this->newsletter->getId(), $this->subscriber->getId(), $customFieldId, $customFieldValue);
+        $wpdb->query($createCustomFieldValueQuery);
+
+        $params = array(
+            "subject" => "This is a test email [!test!]",
+            "htmlbody" => "Test [!test!]",
+            "textbody" => "Test [!test!]",
+            "meta_key" => "TEST-TEST-TEST",
+            'sent' => 1
+        );
+
+        $expected = $params;
+        
+        $expected['textbody'] =$this->getExpectedTextBody($params);
+        $expected['htmlbody'] =$this->getExpectedHtmlBody($params);
+
+        foreach ($expected as $index=>$value) {
+            $expected[$index] = str_replace('[!test!]', $customFieldValue, $value);
+        }
+
+        $email = $javelinQueue->enqueue($this->subscriber, $params);
+
+        $this->assertEquals($expected['subject'], $email->getSubject());
+        $this->assertEquals($expected['textbody'], $email->getTextBody());
+        $this->assertEquals($expected['htmlbody'], $email->getHtmlBody());
     }
 
     public function testWhetherUnsubscriptionTagGetsReplaced()
@@ -165,5 +203,23 @@ class EmailQueueTest extends WP_UnitTestCase
         JavelinTestHelper::deleteAllSubscribers();
         JavelinTestHelper::deleteAllNewsletters();
         JavelinTestHelper::deleteAllMessagesFromQueue();
+    }
+
+    /**
+     * @param $email
+     * @return string
+     */
+    private function getExpectedHtmlBody($email)
+    {
+        return sprintf('%s<br />%s<br /><a href="%s">Click here to unsubscribe.</a>', $email['htmlbody'], $this->senderAddressSetting, $this->subscriber->getUnsubscriptionUrl());
+    }
+
+    /**
+     * @param $email
+     * @return string
+     */
+    private function getExpectedTextBody($email)
+    {
+        return sprintf("%s\r\n%s\r\nClick here to unsubscribe:\r\n%s", $email['textbody'], JavelinConfig::senderAddress(), $this->subscriber->getUnsubscriptionUrl());
     }
 }
